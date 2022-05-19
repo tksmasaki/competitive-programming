310f0bfe2f9073aa18897af9ff479a4cc1ec28e8
tksmasaki <tk.masaki70@gmail.com>
2022-05-19 23:59:58 +0900

    retry coins

--- atcoder/beginners_selection/coins.rb
+++ atcoder/beginners_selection/coins.rb
@@ -1,13 +1,28 @@
-a = gets.chomp.to_i + 1
-b = gets.chomp.to_i + 1
-c = gets.chomp.to_i + 1
-s = gets.chomp.to_i
-r = 0
-a.times do |i|
-  b.times do |j|
-    c.times do |k|
-      r += 1 if (500 * i) + (100 * j) + (50 * k) == s
+a = gets.to_i
+b = gets.to_i
+c = gets.to_i
+x = gets.to_i
+num = 0
+(0..a).each do |i|
+  (0..b).each do |j|
+    (0..c).each do |k|
+      num += 1 if ((500 * i) + (100 * j) + (50 * k)) == x
     end
   end
 end
-puts r
+puts num
+
+# Previous Answer
+# a = gets.chomp.to_i + 1
+# b = gets.chomp.to_i + 1
+# c = gets.chomp.to_i + 1
+# s = gets.chomp.to_i
+# r = 0
+# a.times do |i|
+#   b.times do |j|
+#     c.times do |k|
+#       r += 1 if 500 * i + 100 * j + 50 * k == s
+#     end
+#   end
+# end
+# puts r
